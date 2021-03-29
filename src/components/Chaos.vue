a25260831883ba672a3227869c96bfba2b2bc9ca
yangzq80 <yangzq80@gmail.com>
2021-03-29 12:04:13 +0800

    feat(base):init commit.

---
+++ src/components/Chaos.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'Chaos',
+    data () {
+      return {
+        msg: 'Welcome to chaos rules App'
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
